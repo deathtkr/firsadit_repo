f6443fe7870e17e49daaa7fa9af3b4ef026e107b
deathtkr <64887306+deathtkr@users.noreply.github.com>
2024-06-11 13:13:56 +0530

    Update providers.tf

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,5 +1,9 @@
 provider "azurerm" {
   features {}
+  subscription_id = $subscription_id
+  client_id       = $client_id
+  client_secret   = $client_secret
+  tenant_id       = $tenant_id
 }
 
 terraform {
