392058d65b6fbb421800f676487bd4d3cf27c51c
deathtkr <64887306+deathtkr@users.noreply.github.com>
2024-07-01 11:42:44 +0530

    Add files via upload

---
+++ terraform/variables.tf
@@ -0,0 +1,46 @@
+variable "resource_group_name" {
+  type        = string
+  description = "RG name in Azure"
+}
+variable "location" {
+  type        = string
+  description = "Resources location in Azure"
+}
+variable "cluster_name" {
+  type        = string
+  description = "AKS name in Azure"
+}
+variable "kubernetes_version" {
+  type        = string
+  description = "Kubernetes version"
+}
+variable "system_node_count" {
+  type        = number
+  description = "Number of AKS worker nodes"
+}
+
+variable "user_node" {
+  type        = number
+  description = "Number of AKS worker nodes"
+}
+
+variable "subscription_id" {
+  description = "Azure Subscription ID"
+  type        = string
+}
+
+variable "client_id" {
+  description = "Azure Client ID"
+  type        = string
+}
+
+variable "client_secret" {
+  description = "Azure Client Secret"
+  type        = string
+}
+
+variable "tenant_id" {
+  description = "Azure Tenant ID"
+  type        = string
+}
+
