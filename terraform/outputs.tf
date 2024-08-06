e1568d2b39cbfe796d9ad091e4514268702d2d71
Deathtkr <adityasaxena34244@gmail.com>
2024-08-06 15:28:33 +0530

    na

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,7 +2,9 @@ output "cluster_id" {
   description = "EKS cluster ID."
   value       = module.eks.cluster_id
 }
-
+output "cluster_name" {
+  value = module.eks.cluster_name
+}
 output "cluster_endpoint" {
   description = "Endpoint for EKS control plane."
   value       = module.eks.cluster_endpoint
@@ -21,9 +23,4 @@ output "region" {
 output "oidc_provider_arn" {
   value = module.eks.oidc_provider_arn
 }
-
-#output "zz_update_kubeconfig_command" {
-  # value = "aws eks update-kubeconfig --name " + module.eks.cluster_id
-#  value = format("%s %s %s %s", "aws eks update-kubeconfig --name", module.eks.cluster_id, "--region", var.aws_region)
-#}
 
